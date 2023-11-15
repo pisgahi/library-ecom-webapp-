05d7a4c07d2576cb97876a239bc4ff49199f65ac
Pisgah <pisgah2002@gmail.com>
2023-11-15 03:12:12 -0500

    fix images

--- src/data.js
+++ src/data.js
@@ -1,9 +1,23 @@
+import Interview from "./assets/crack the coding interview.png";
+import Goggins from "./assets/david goggins.jpeg";
+import Habits from "./assets/atomic habits.jpg";
+import Work from "./assets/deep work.jpeg";
+import Ten from "./assets/book-1.jpeg";
+import Obsessed from "./assets/book-2.jpeg";
+import Rich from "./assets/book-3.jpeg";
+import CashFlow from "./assets/book-4.jpeg";
+import Power from "./assets/book-5.jpeg";
+import Five from "./assets/book-6.jpeg";
+import Mastery from "./assets/book-8.jpeg";
+import Influence from "./assets/book-9.jpg";
+import Sell from "./assets/book-10.jpg";
+
 // FAKE DATA
 export const books = [
   {
     id: 1,
     title: "Crack the Coding Interview",
-    url: "https://covers.openlibrary.org/b/id/8091016-L.jpg",
+    url: `${Interview}`,
     originalPrice: 49.95,
     salePrice: 14.95,
     rating: 4.5,
@@ -11,7 +25,7 @@ export const books = [
   {
     id: 2,
     title: "Atomic Habits",
-    url: "https://covers.openlibrary.org/b/id/10958382-L.jpg",
+    url: `${Habits}`,
     originalPrice: 39,
     salePrice: null,
     rating: 5,
@@ -19,7 +33,7 @@ export const books = [
   {
     id: 3,
     title: "Can't Hurt Me",
-    url: "https://covers.openlibrary.org/b/id/10425061-L.jpg",
+    url: `${Goggins}`,
     originalPrice: 29,
     salePrice: null,
     rating: 5,
@@ -27,7 +41,7 @@ export const books = [
   {
     id: 4,
     title: "Deep Work",
-    url: "https://covers.openlibrary.org/b/id/10088428-L.jpg",
+    url: `${Work}`,
     originalPrice: 44,
     salePrice: 19,
     rating: 4.5,
@@ -35,7 +49,7 @@ export const books = [
   {
     id: 5,
     title: "The 10X Rule",
-    url: "https://covers.openlibrary.org/b/id/9978588-L.jpg",
+    url: `${Ten}`,
     originalPrice: 32,
     salePrice: null,
     rating: 5,
@@ -43,7 +57,7 @@ export const books = [
   {
     id: 6,
     title: "Sell Or Be Sold",
-    url: "https://covers.openlibrary.org/b/id/7737110-L.jpg",
+    url: `${Sell}`,
     originalPrice: 70,
     salePrice: 12.5,
     rating: 5,
@@ -51,7 +65,7 @@ export const books = [
   {
     id: 7,
     title: "Rich Dad Poor Dad",
-    url: "https://covers.openlibrary.org/b/id/2380224-L.jpg",
+    url: `${Rich}`,
     originalPrice: 11,
     salePrice: 10,
     rating: 4,
@@ -59,7 +73,7 @@ export const books = [
   {
     id: 8,
     title: "Cashflow Quadrant",
-    url: "https://covers.openlibrary.org/b/id/1954899-L.jpg",
+    url: `${CashFlow}`,
     originalPrice: 38,
     salePrice: 17.95,
     rating: 4.5,
@@ -67,7 +81,7 @@ export const books = [
   {
     id: 9,
     title: "48 Laws of Power Summary",
-    url: "https://covers.openlibrary.org/b/id/8906626-L.jpg",
+    url: `${Power}`,
     originalPrice: 35,
     salePrice: 19.95,
     rating: 4.5,
@@ -75,7 +89,7 @@ export const books = [
   {
     id: 10,
     title: "The 5 Second Rule",
-    url: "https://covers.openlibrary.org/b/id/8114155-L.jpg",
+    url: `${Five}`,
     originalPrice: 40,
     salePrice: null,
     rating: 5,
@@ -83,7 +97,7 @@ export const books = [
   {
     id: 11,
     title: "How to Win Friends & Influence People",
-    url: "https://covers.openlibrary.org/b/id/7895280-L.jpg",
+    url: `${Influence}`,
     originalPrice: 30,
     salePrice: 20,
     rating: 5,
@@ -91,7 +105,7 @@ export const books = [
   {
     id: 12,
     title: "Mastery",
-    url: "https://covers.openlibrary.org/b/id/8479576-L.jpg",
+    url: `${Mastery}`,
     originalPrice: 30,
     salePrice: 12.95,
     rating: 4.5,
